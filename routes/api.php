9cb8f9d20d07612bc1d1d05d3d9cc94d9bb2016c
Clement Barbaza <clement.barbaza@gmail.com>
2021-05-10 09:29:51 +0200

    init

---
+++ routes/api.php
@@ -0,0 +1,10 @@
+<?php
+
+use App\Http\Controllers\ApiTokenController;
+use Illuminate\Support\Facades\Route;
+
+// Auth
+Route::post('auth/register', [ApiTokenController::class, 'register']);
+Route::post('auth/login', [ApiTokenController::class, 'login']);
+Route::middleware('auth:sanctum')->post('auth/me', [ApiTokenController::class, 'me']);
+Route::middleware('auth:sanctum')->post('auth/logout', [ApiTokenController::class, 'logout']);
